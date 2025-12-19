0d4b51135a7e18e8db9c05d3421e7936a9f4565f
Rahul Goswami <therahulgoswami99@gmail.com>
2025-12-20 03:51:22 +0530

    updated

--- main.js
+++ main.js
@@ -124,3 +124,5 @@ document.addEventListener("DOMContentLoaded", () => {
 //   }
 // });
 
+
+
---
+++ validate.js
@@ -0,0 +1,45 @@
+// name validation
+const nameValid = () => {
+    var name = document.getElementById('name').value;
+    var nameRegex = /^[A-Z a-z]{3,30}$/;
+
+    if (nameRegex.test(name)) {
+        document.getElementById("errorName").innerHTML = "";
+        document.getElementById("btn").disabled = false;
+    } else {
+        document.getElementById("errorName").innerHTML = "⚠️Please enter a valid name";
+        document.getElementById("btn").disabled = true;
+    }
+}
+
+// email validation
+const emailValid = () => {
+    var email = document.getElementById('email').value;
+    var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (emailRegex.test(email)) {
+        document.getElementById("errorEmail").innerHTML = "";
+        document.getElementById("btn").disabled = false;
+    } else {
+        document.getElementById("errorEmail").innerHTML = "⚠️Please enter a valid email";
+        document.getElementById("btn").disabled = true;
+    }
+
+}
+
+
+//  phone number validation
+const phoneValid = () => {
+    var phone = document.getElementById('phone').value;
+    var phoneRegex = /^(?:\+91[\-\s]?)?[6-9]\d{9}$/;
+
+    if (phoneRegex.test(phone)) {
+        document.getElementById("errorPhone").innerHTML = "";
+        document.getElementById("btn").disabled = false;
+    } else {
+        document.getElementById("errorPhone").innerHTML = "⚠️Please enter a valid phone number";
+        document.getElementById("btn").disabled = true;
+    }
+
+}
+
